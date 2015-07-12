840138b1b308c84f513e88066a1f1f86958a160c
Tareq Fadel <itrainit@gmail.com>
2015-07-12 19:29:12 +0100

    Add Change to Manifest to avoid network calls for Tracks when orientation change

--- app/src/main/java/uk/tareq/spotifystreamer/TrackActivityFragment.java
+++ app/src/main/java/uk/tareq/spotifystreamer/TrackActivityFragment.java
@@ -3,6 +3,7 @@ package uk.tareq.spotifystreamer;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -36,6 +37,28 @@ public class TrackActivityFragment extends Fragment {
     public TrackActivityFragment() {
     }
 
+    /**
+     * custom onSaveInstanceState, checks if the adapter is populated. If it is, creates a
+     * Parcelable Array and copies all ListView items from the adapter into the Parcelable
+     *
+     * @param outState Bundle that places the Parcelable array with the key
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if (mTrackAdapter.getCount() > 0) { // check if adapter contains items
+            // Construct a new Parcelable Array the same size as Custom ArrayAdapter
+            Parcelable[] parcelablesT = new Parcelable[mTrackAdapter.getCount()];
+
+            // Loop through the items in the array adapter and place each into the parcelables array
+            for (int i = 0; i < mTrackAdapter.getCount(); i++) {
+                parcelablesT[i] = mTrackAdapter.getItem(i);
+            }
+            // put the parcelable array with the key "track" ready for retrieval tempStorage
+            outState.putParcelableArray("track", parcelablesT);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -55,6 +78,21 @@ public class TrackActivityFragment extends Fragment {
         mTrackAdapter = new TrackAdapter(getActivity(), R.layout.list_item_track, trackList);
         mTrackListView = (ListView) rootView.findViewById(R.id.list_view_tracks);
         mTrackListView.setAdapter(mTrackAdapter);
+
+        // Parcelables section
+        if (savedInstanceState != null) { // check that there is a savedInstanceState
+
+            // Get the parcelables from the savedInstanceState with the key "track" and store it in
+            // a Parcelable Array
+            Parcelable[] parcelablesT = savedInstanceState.getParcelableArray("track");
+
+            if (parcelablesT != null) { // run if parcelables contains items
+                // for each item in parcelable add it back to the custom artist array adapter
+                for (Parcelable parcelable : parcelablesT)
+                    mTrackAdapter.add(((MyTrack) parcelable));
+            }
+        }
+
 
         new Top10TrackTask().execute(mArtistId); // run AsyncTask
 

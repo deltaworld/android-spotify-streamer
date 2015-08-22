a1acb6582d462e167c36e11c62700b555b8e26da
Tareq Fadel <itrainit@gmail.com>
2015-08-22 23:18:23 +0100

    Add ClickListener to PlayerUI

--- app/src/main/java/uk/tareq/spotifystreamer/ActivityFragment/TrackActivityFragment.java
+++ app/src/main/java/uk/tareq/spotifystreamer/ActivityFragment/TrackActivityFragment.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
@@ -112,6 +113,14 @@ public class TrackActivityFragment extends Fragment {
         } else {
             Utils.giveToastMessage(getActivity(), getResources().getString(R.string.no_internet));
         }
+
+        // Add ClickListener to player
+        mTrackListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(LOG_TAG, "onItemClick ");
+            }
+        });
         return rootView;
     }
 

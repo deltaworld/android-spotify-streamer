621cf58555941ff2ed4ca3a734b056c1ad46e53f
Tareq Fadel <itrainit@gmail.com>
2015-08-23 15:30:01 +0100

    SeekBar Scrubbable

--- app/src/main/java/uk/tareq/spotifystreamer/ActivityFragment/PlayerActivityFragment.java
+++ app/src/main/java/uk/tareq/spotifystreamer/ActivityFragment/PlayerActivityFragment.java
@@ -2,6 +2,7 @@ package uk.tareq.spotifystreamer.ActivityFragment;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,11 +10,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import uk.tareq.spotifystreamer.R;
 
@@ -23,10 +26,35 @@ import uk.tareq.spotifystreamer.R;
 public class PlayerActivityFragment extends Fragment {
 
     private static final String LOG_TAG = PlayerActivityFragment.class.getSimpleName();
-    private boolean mPlaying = false;
     private MediaPlayer mMediaPlayer = new MediaPlayer();
+    private int mTimeElapsed = 0;
+    private int mDuration;
+    private int mForwardTime = 2000, mBackwardTime = 2000;
+    private SeekBar mSeekBar;
+    private Handler mDurationHandler = new Handler();
 
+    private TextView mStartTimer;
+    private ImageButton mForward;
+    private ImageButton mBackward;
+    // http://examples.javacodegeeks.com/android/android-mediaplayer-example/
+    // handler to change seekBarTime
+    private Runnable updateSeekBarTime = new Runnable() {
+        public void run() {
+            //get current position
+            mTimeElapsed = mMediaPlayer.getCurrentPosition();
+            //set seekbar progress
+            mSeekBar.setProgress(mTimeElapsed);
 
+            mStartTimer.setText(String.format("%d:%02d",
+                    TimeUnit.MILLISECONDS.toMinutes((long) mTimeElapsed),
+                    TimeUnit.MILLISECONDS.toSeconds((long) mTimeElapsed) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)
+                                    mTimeElapsed))));
+
+            //repeat yourself that again in 100 miliseconds
+            mDurationHandler.postDelayed(this, 100);
+        }
+    };
 
     public PlayerActivityFragment() {
     }
@@ -37,6 +65,7 @@ public class PlayerActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_player, container, false);
 
         Bundle extras = getActivity().getIntent().getExtras();
+
 
         String artistId = extras.getString("EXTRA_ARTIST_ID");
         String artistName = extras.getString("EXTRA_ARTIST_NAME");
@@ -62,8 +91,10 @@ public class PlayerActivityFragment extends Fragment {
 
         // UI: Player functionality
         final ImageButton playButton = (ImageButton) rootView.findViewById(R.id.play_pause_image_button);
-        ImageButton previousButton = (ImageButton) rootView.findViewById(R.id.previous_track_image_button);
-        ImageButton nextButton = (ImageButton) rootView.findViewById(R.id.next_track_image_button);
+        final ImageButton previousButton = (ImageButton) rootView.findViewById(R.id.previous_track_image_button);
+        final ImageButton nextButton = (ImageButton) rootView.findViewById(R.id.next_track_image_button);
+        mSeekBar = (SeekBar) rootView.findViewById(R.id.scrub_seek_bar);
+        mStartTimer = (TextView) rootView.findViewById(R.id.start_timer_text_view);
 
         // UI: Static fields
         albumNameTextView.setText(albumName);
@@ -81,7 +112,9 @@ public class PlayerActivityFragment extends Fragment {
         try {
             mMediaPlayer.setDataSource(trackUrl);
             mMediaPlayer.prepare();
-            int duration = mMediaPlayer.getDuration();
+            mDuration = mMediaPlayer.getDuration();
+            mSeekBar.setMax(mDuration);
+
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -95,6 +128,7 @@ public class PlayerActivityFragment extends Fragment {
                 if (!mMediaPlayer.isPlaying()) {
                     mMediaPlayer.start();
                     playButton.setImageResource(android.R.drawable.ic_media_pause);
+                    mDurationHandler.postDelayed(updateSeekBarTime, 100);
                 } else {
                     mMediaPlayer.pause();
                     playButton.setImageResource(android.R.drawable.ic_media_play);
@@ -120,10 +154,25 @@ public class PlayerActivityFragment extends Fragment {
             }
         });
 
+        // http://stackoverflow.com/questions/17168215/seekbar-and-media-player-in-android
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (mMediaPlayer != null && fromUser) {
+                    mMediaPlayer.seekTo(progress);
+                }
+            }
+        });
 
 
         return rootView;
     }
-
-
 }
